30f8ade39b19890ad8769657ba7b6b2b46089d3c
HXgrowns <804156017>
2020-01-10 22:16:23 +0800

    [huxiao] {add}: add delete confirm

---
+++ js/index.js
@@ -0,0 +1,9 @@
+const seletedDelete = () => {
+  let DeleteLayout = document.getElementById("isDelete");
+  DeleteLayout.style.display = "block";
+}
+
+const unseleteDelete = () => {
+  let DeleteLayout = document.getElementById("isDelete");
+  DeleteLayout.style.display = "none";
+}
